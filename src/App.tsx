2e2f7dd4024df22e6eabe2b291503916104b361a
Dmitry Andriyanov <dima117a@yandex-team.ru>
2022-07-25 18:32:20 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,166 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC } from 'react';
+import { Column, useTable } from 'react-table';
+import { Employee, STUB } from './stub';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const COLUMNS: Column<Employee>[] = [
+    {
+        Header: 'Фамилия, Имя',
+        accessor: x => (
+            <div>
+                <span>{x.name}</span>
+                <span>@{x.login}</span>
+            </div>
+        )
+    },
+    {
+        Header: 'Column 2',
+        accessor: x => <span>{x.salary}₽</span>,
+    },
+];
+
+export const App: FC = () => {
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable({
+        columns: COLUMNS,
+        data: STUB,
+    });
+
+    return (<div className='container'>
+        <h1>qwfqwefqwef</h1>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <table className="table" {...getTableProps()}>
+            <thead>
+                {// Loop over the header rows
+                    headerGroups.map(headerGroup => (
+                        // Apply the header row props
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {// Loop over the headers in each row
+                                headerGroup.headers.map(column => (
+                                    // Apply the header cell props
+                                    <th {...column.getHeaderProps()}>
+                                        {// Render the header
+                                            column.render('Header')}
+                                    </th>
+                                ))}
+                        </tr>
+                    ))}
+            </thead>
+            {/* Apply the table body props */}
+            <tbody {...getTableBodyProps()}>
+                {// Loop over the table rows
+                    rows.map(row => {
+                        // Prepare the row for display
+                        prepareRow(row)
+                        return (
+                            // Apply the row props
+                            <tr {...row.getRowProps()}>
+                                {// Loop over the rows cells
+                                    row.cells.map(cell => {
+                                        // Apply the cell props
+                                        return (
+                                            <td {...cell.getCellProps()}>
+                                                {// Render the cell contents
+                                                    cell.render('Cell')}
+                                            </td>
+                                        )
+                                    })}
+                            </tr>
+                        )
+                    })}
+            </tbody>
+        </table>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+        <p>qwg4rt4yt</p>
+
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/stub.ts
@@ -0,0 +1,44 @@
+
+const loginParts: string[] = ['ko', 'man', 'di', 'mi', 'a', 'le', 'ku', 'ged', 'an']
+const names1: string[] = ['Игорь', 'Сергей', 'Михаил', 'Иван', 'Владимир', 'Николай'];
+const names2: string[] = [
+    'Смирнов',
+    'Иванов',
+    'Кузнецов',
+    'Соколов',
+    'Попов',
+    'Лебедев',
+    'Козлов',
+    'Новиков',
+    'Морозов',
+    'Петров',
+    'Волков',
+    'Соловьёв',
+    'Васильев',
+    'Зайцев',
+    'Павлов',
+    'Семёнов',
+    'Голубев',
+    'Виноградов'
+];
+
+const random = (max: number): number => Math.floor(Math.random() * max);
+const getRandomElem = <T>(a: T[]): T => a[random(a.length)];
+
+export interface Employee { 
+    id: number;
+    name: string;
+    login: string;
+    salary: number;
+}
+
+function getRandomEmployee(_: unknown, id: number): Employee {
+    const name = `${getRandomElem(names1)} ${getRandomElem(names2)}`;
+    const login = new Array(2 + random(3)).fill(true).map(() =>getRandomElem(loginParts)).join('') + id;
+
+    return { id, name, login, salary: 1000 * (100 + random(200)) };
+}
+
+export const STUB: Employee[] = new Array(496)
+    .fill(true)
+    .map(getRandomEmployee);
